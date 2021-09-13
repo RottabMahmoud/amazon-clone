c1c5221e02cd7020f11adbd0f0dc33fb7ade2c1e
Mahmoud <mahmoud@Mahmouds-MacBook-Pro.local>
2021-09-13 22:39:00 +0200

    2nd commit

--- src/Product.js
+++ src/Product.js
@@ -1,5 +1,22 @@
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
+
 function Product({ id, title, image, price, rating }) {
+  const [{ basket }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    // dispatch the item into the data later
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id,
+        title,
+        image,
+        price,
+        rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -17,7 +34,7 @@ function Product({ id, title, image, price, rating }) {
         </div>
       </div>
       <img src={image} alt="" />
-      <button>Add to basket</button>
+      <button onClick={addToBasket}>Add to basket</button>
     </div>
   );
 }
